0030949e5312fedb3eeed5e59fc1ae3c7812a1eb
Qasim Khan <78582000+qasimk2120@users.noreply.github.com>
2024-07-25 06:39:47 +0300

    Adding application to git with current implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, UserComponent],
 })
 export class AppComponent {
-  title = 'angular-new-updated';
+  users = DUMMY_USERS;
+
+
+  onSelectUser(id:number){
+    console.log('User Selected with UserId: ' + id)
+  }
 }
